3f53601abe40c62f1519cbfa7ec9a004dfa17a65
Paula <paula020300@gmail.com>
2026-08-19 23:00:47 +0200

    Clean up SQL queries

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,7 +1,5 @@
 /* 
 Question: What are the top-paying data analyst jobs?
-- Identify the top 10 highest-paying Data Analyst roles that are available remotely.
-- Focuses on job postings with specified salaries (remove nulls).
 - Why? Highlight the top-paying opportunities for Data Analysts, offering insights
 */
 
